47fe53ba1a563c3e434bc1ed6518e0efa47cf3d1
John <johnfuhrmeister12@gmail.com>
2024-06-02 15:35:34 -0400

    Fetch vantage API stock data and correctly format for chart

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,41 @@ function App() {
 
   const alphaVantageKey = process.env.REACT_APP_ALPHA_VANTAGE_API_KEY; // .env file public for demo use
 
+  function extractStockData(data:any) {
+    const extractedData:any = [];
+
+    // Extract neeeded values for chart from res data
+    if (data['Time Series (5min)']) {
+      Object.entries(
+        data['Time Series (5min)']
+      ).map((key:any, value:any) => {
+        extractedData.push({
+          x: key,
+          y: [
+            value['1. open'],
+            value['2. high'],
+            value['3. low'],
+            value['4. close']
+          ]
+        })
+      })
+    }
+
+    return extractedData;
+  }
+
   const getStockData = async (ticker:string) => {
-    axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${alphaVantageKey}`).then((res) => {
-      console.log(res)
+    await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${alphaVantageKey}`).then((res) => {
+      const vantageData = res.data;
+      const formattedData = extractStockData(vantageData);
+      setStockData(formattedData);
+      console.log(formattedData)
     })
   }
 
   const getFinancials = async () => {
     // Docker Container - Flask API
-    axios.get('http://127.0.0.1:8000/financials').then((res) => {
+    await axios.get('http://127.0.0.1:8000/financials').then((res) => {
       const localFinancials = res.data;
       setAnalysts(localFinancials);
       setRatios(localFinancials);
